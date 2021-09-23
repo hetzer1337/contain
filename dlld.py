d5344d1b2e66cfdf86058728c63614a52879fac0
Gubarev Timophey <tg.gubarev@gmail.com>
2021-09-23 19:08:36 +0300

    dd

---
+++ dlld.py
@@ -0,0 +1,9 @@
+
+product = input("vvedite produkt")
+if product == 'milk':
+        print(3)
+elif product == 'butter':
+        print(5)
+elif product == 'bread':
+        print(7)
+print(product)
--- pask.py
+++ pask.py
@@ -1,1 +1,1 @@
-print("dd")
+print(hash('abcde'))
